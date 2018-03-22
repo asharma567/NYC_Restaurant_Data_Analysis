1aeabb91072403ba24b8ab2b834cebfd211e9fbd
Ajay <asharma567567@gmail.com>
2018-03-22 06:50:04 -0400

    ad

--- pull_lat_lon_for_all_DBA.py
+++ pull_lat_lon_for_all_DBA.py
@@ -2,7 +2,7 @@ from helpers import *
 import pandas as pd
 import joblib
 
-df = pd.read_csv('/Users/ajaysharma/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv')
+df = pd.read_csv('~/Downloads/DOHMH_New_York_City_Restaurant_Inspection_Results.csv')
 df = df.drop_duplicates(["DBA"])
 
 lat_lons = {}
@@ -11,24 +11,36 @@ dba =[]
 addresses = []
 coords=[]
 
-for i, row in enumerate(df[["DBA",'BUILDING', 'STREET', 'BORO','ZIPCODE']].to_records()[::-1]):
-    addr = \
-        str(row[1]) + ',' + \
-        str(row[2]) + ' ' + \
-        str(row[3]) + ',' +  \
-        str(row[4]) + ',' + \
-        str(row[5])[:-2]
+lat_lons = joblib.load('lat_lons.pkl')
 
-    addresses.append(addr)
-    dba.append(row[1])
-    idx.append(row[0])
-    lat_lons[addr] = get_lat_lon(addr)
+for i, row in enumerate(df[["DBA",'BUILDING', 'STREET', 'BORO','ZIPCODE']].to_records()):
+    try:
+        addr = \
+            str(row[1]) + ',' + \
+            str(row[2]) + ' ' + \
+            str(row[3]) + ',' +  \
+            str(row[4]) + ',' + \
+            str(row[5])[:-2]
 
-    coords.append(lat_lons[addr])
-    joblib.dump(lat_lons, 'lat_lons.pkl')
+
+        addresses.append(addr)
+        dba.append(row[1])
+        idx.append(row[0])
+        
+
+        if lat_lons.get(addr): 
+            continue
+
+        lat_lons[addr] = get_lat_lon(addr)
+        coords.append(lat_lons[addr])
+        joblib.dump(lat_lons, 'lat_lons.pkl')
+
+        print (i)
     
-
-
+    except:
+        print ('CRASHED!')
+        joblib.dump(lat_lons, 'lat_lons.pkl')
+    
 
 
 
